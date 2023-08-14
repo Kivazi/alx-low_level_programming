e6d6d10d20dcfa1ff379c518a1f566ee625844d7
peter <petrkat8ba@gmail.com>
2023-08-14 01:09:42 -0700

    mandatory tasks

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - begining of the function
+ *
+ * Return: returns zero
+ */
+
+int main(void)
+{
+  puts("\"Programming is like building a multilingual puzzle");
+  
+  return(0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * MAIN: fuction to print 
+ *
+ * RETURN: returns zero
+ */
+int main(void)
+{
+  printf("with proper grammar, but the outcome is a piece of art,\n");
+
+  return(0);
+}
